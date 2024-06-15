9a94ae8fe66ad8dc1cc11883e15186cb75873935
Risyad Ridwansyah <risyadridwansyah@gmail.com>
2024-06-16 02:51:56 +0700

    menambahkan tabel database users dan koleksi

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id_user');
+            $table->string('username');
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('nama_lengkap');
+            $table->enum('lvl_akses', ['admin', 'kurator', 'peneliti', 'pengunjung', 'staff']);
             $table->rememberToken();
             $table->timestamps();
         });
